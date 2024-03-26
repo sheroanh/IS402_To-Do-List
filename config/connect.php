bf122003d5b435847299d98c7b1a98a974f54a35
sheoanh <anhnguyentuan8@gmail.com>
2024-03-26 10:22:30 +0700

    [26/3] update config

--- config/connect.php
+++ config/connect.php
@@ -13,7 +13,7 @@ class Connect
     public function __construct()
     {
         $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
-        $dotenv->load();
+        $dotenv->safeLoad();
         $this->server = $_ENV['DB_HOST'];
         $this->username = $_ENV['DB_USERNAME'];
         $this->password = $_ENV['DB_PASSWORD'];
@@ -25,6 +25,34 @@ class Connect
         if ($connect->connect_error) {
             die ("Connection failed: " . $connect->connect_error);
         }
+        $sql = "SELECT * 
+        FROM information_schema.tables
+        WHERE table_schema = '$this->dbname' 
+            AND table_name = 'user'
+        LIMIT 1";
+        $result = $connect->query($sql);
+        if ($result->num_rows == 0) {
+            $this->initializeDatabase($connect);
+        }
         return $connect;
+    }
+
+    public function initializeDatabase($connect)
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS user (
+            username varchar(50) NOT NULL,
+            password varchar(50) DEFAULT NULL
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci";
+        $connect->query($sql);
+        $sql = "CREATE TABLE IF NOT EXISTS task (
+            id int(11) NOT NULL,
+            title mediumtext DEFAULT NULL,
+            description mediumtext DEFAULT NULL,
+            username varchar(50) DEFAULT NULL,
+            is_done tinyint(1) NOT NULL DEFAULT 0,
+            created_at timestamp NOT NULL DEFAULT current_timestamp(),
+            updated_at timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci";
+        $connect->query($sql);
     }
 }
